4da9bdf4ccace9bc4b770b601e30c16729fa571b
Jithesh Kumar M S <jitheshkumarjithu8@gmail.com>
2026-02-23 15:22:37 +0000

    add career transition output message

--- first-shell-script.sh
+++ first-shell-script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #echo "My name is Jithesh"
 echo -e "\e[1;32m🚀 Jithesh — journeying from Mobile App Dev to Cloud & DevOps Engineer ☁️\e[0m"
+echo "📈 Skills upgrading in progress… Mobile ➜ Cloud DevOps"
